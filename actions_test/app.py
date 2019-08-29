1aad04849c5a0e6fda7d18cd067bdca71ef9161c
Brennan Magee <brennan@xmos.com>
2019-08-29 12:53:53 +0100

    Fix imports and add tests

--- actions_test/app.py
+++ actions_test/app.py
@@ -4,18 +4,26 @@ import sys
 def wrap_string(to_wrap, border='#', full=False,
                 top=False, sides=False, bottom=True):
     """Take a string and return it with borders"""
+    if len(to_wrap) == 0:
+        return ''
     if full:
         bottom, top, sides = True, True, True
     result = ''
-    length = len(to_wrap) // len(border)
+    lines = to_wrap.splitlines()
+    longest_line = max(lines, key=lambda x: len(x))
+    length = len(longest_line) // len(border)
+
     if sides:
         length += 2 // len(border) + 2
     if top:
         result += border * length
-    if sides:
-        result += '\n{0} {1} {0}\n'.format(border, to_wrap)
-    else:
-        result += '\n{}\n'.format(to_wrap)
+    for line in lines:
+        if sides:
+            padding = length - len(line) - 2 * len(border) - 1  # other space
+            result += '\n{0} {1}{2}{0}'.format(border, line, ' '*padding)
+        else:
+            result += '\n{}'.format(line)
+    result += '\n'
     if bottom:
         result += border * length
 
@@ -25,3 +33,4 @@ def wrap_string(to_wrap, border='#', full=False,
 if __name__ == '__main__':
     print()
     print(wrap_string(' '.join(sys.argv[1:]), full=True))
+    print(wrap_string('multiline\nstring', full=True))
--- test/test_app.py
+++ test/test_app.py
@@ -1,12 +1,14 @@
 import pytest
-from app import wrap_string
+from ..actions_test.app import wrap_string
 
 
 class TestWrapString():
     @pytest.mark.parametrize("word, expected", [
+        ['', ''],
         ['hello', '#########\n# hello #\n#########'],
         ['hello again', '###############\n# hello again #\n###############'],
         ['hello\nover\nlines', '#########\n# hello #\n# over  #\n# lines #\n#########'],
+        ['\nskip\nfirst\nline', '#########\n#       #\n# skip  #\n# first #\n# line  #\n#########'],
     ])
     def test_wrap_string_full(self, word, expected):
         result = wrap_string(word, full=True)
